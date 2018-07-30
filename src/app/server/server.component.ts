72c21c8d8e8eb3abd1c7cf60ef60d1c5f385f332
Kevin <kevaschm713@gmail.com>
2018-07-30 02:39:13 -0400

    property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html' 
+    templateUrl: './server.component.html',
 })
 
 export class ServerComponent {
-    serverId: number = 10;
-    serverStatus: string = "online";
+    serverId = 10;
+    serverStatus = 'online';
 
-    getServerStatus(){
+    getServerStatus() {
         return this.serverStatus;
     }
 }
